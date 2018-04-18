e8babeb3a086082a736434901c27a5b33dfd940f
aaqqxx <aaqqxx1910@gmail.com>
2018-04-18 11:10:54 +0800

    1.简单串口读写程序。一次将缓冲区的数据读完。

--- serial_example/simple_serial_example.py
+++ serial_example/simple_serial_example.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-#!/usr/bin/env python
+# !/usr/bin/env python
 
 __author__ = 'XingHua'
 
@@ -8,10 +8,17 @@ __author__ = 'XingHua'
 """
 
 import serial
-ser = serial.Serial()
-ser.baudrate = 38400
-ser.port = 0
-ser.open()
-# ser.read(5)
-# ser.write("hello")
-print ser.read(5)
+from time import sleep
+
+ser = serial.Serial('COM1', 9600, timeout=5)
+
+cmd_list = ["cmd13\r\ncmd14\r\n", "cmd2", "cmd3", "cmd4"]
+res_length_list = [4, 4, 4, 4]
+
+while True:
+    for cmd, res_length in zip(cmd_list, res_length_list):
+        ser.write(cmd)
+        sleep(0.1)
+        data = ser.read_all()
+        print data
+        sleep(1)
